2ce2015f157516dac19df186b0b0229ea23b1d44
Subhash Bose <online@subhashbose.com>
2025-05-17 16:29:33 +0200

    fix

--- http-helloworld.go
+++ http-helloworld.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// Define port flag with default value of 8080
 	text := flag.String("text", "Hello, World!", "Text body to serve")
-	addr := *flag.String("addr", "8080", "Address to run the server on. This can a full address in IP:PORT format or just a PORT listening to all adresses.")
+	addr := flag.String("addr", "8080", "Address to run the server on. This can a full address in IP:PORT format or just a PORT listening to all adresses.")
 	flag.Parse()
 
 	// Define handler for root path
@@ -20,11 +20,11 @@ func main() {
 	})
 
 	// Start server on specified port
-	if(strings.Index(addr, ":") < 0){
-		addr = ":" + addr
+	if(strings.Index(*addr, ":") < 0){
+		*addr = ":" + *addr
 	}
-	log.Printf("Server starting on address %s", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	log.Printf("Server starting on address %s", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }

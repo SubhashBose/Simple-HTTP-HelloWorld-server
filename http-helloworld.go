99d246a5fe5e1864eddd53943ed2c1c7d2d577f2
Subhash Bose <online@subhashbose.com>
2025-05-18 02:18:32 +0200

    added logging of hits

--- http-helloworld.go
+++ http-helloworld.go
@@ -12,10 +12,14 @@ func main() {
 	// Define port flag with default value of 8080
 	text := flag.String("text", "Hello, World!", "Text body to serve")
 	addr := flag.String("addr", "8080", "Address to run the server on. This can a full address in IP:PORT format or just a PORT listening to all adresses.")
+	logging := flag.Bool("log", false, "Enable logging of all incoming hits.")
 	flag.Parse()
 
 	// Define handler for root path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if(logging) {
+			log.Printf("Received hit from %s", r.RemoteAddr)
+		}
 		w.Header().Set("Server", "HTTP HelloWorld Server by S. Bose")
 		fmt.Fprintf(w, *text)
 	})
